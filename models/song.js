78695f39f82bc073f1fbf846b50be041fb7233d4
Wisbell <westleyisbell@gmail.com>
2017-04-20 15:26:24 -0500

    Created song model

--- models/song.js
+++ models/song.js
@@ -4,9 +4,39 @@ const { bookshelf } = require('../db/database')
 // <Define a model using bookshelf that describes a song object, as well as
 // static methods for getting one or all songs from the db>
 
+const Song = bookshelf.Model.extend({
+  tableName: 'Song',
+}, {
+  getAllSongs: () => {
+    console.log("Get all songs function called")
+    return this.forge()
+    .fetchAll()
+    .then( (rows) => {
+      return rows
+    })
+    .catch( (error) => {
+      return error
+    })
+  },
 
+  getSingleSong: (id) => {
+    console.log("Get single song function called")
+    return this.forge({id})
+    .fetch()
+    .then( (song) => {
+      return song
+    })
+    .catch( (error) => {
+      return error
+    })
+  }
+})
 
 
 // When adding the 'table' property use your old friend DB Browser for SQLite to
 // open up the musichistory db and inspect the tables. Add the appropriate table name for
 // interfacing with the songs collection
+
+
+
+module.exports = bookshelf.model('Song', Song)
